4806097f4211ed3a336a6af43d5a29d8f72d0da6
tomonakar <tomohisa.nak@gmail.com>
2018-02-05 19:51:01 +0900

    メモ詳細コンポーネントを追加

--- memoApp/src/elements/CircleButton.js
+++ memoApp/src/elements/CircleButton.js
@@ -3,9 +3,18 @@ import {StyleSheet, View, Text} from 'react-native'
 
 class CircleButton extends React.Component {
   render() {
+    const {style, color} = this.props
+
+    let bgColor = '#e31676'
+    let textColor = '#fff'
+
+    if (color === 'white') {
+      bgColor = '#fff'
+      textColor = '#e31676'
+    }
     return (
-      <View style={styles.CircleButton}>
-        <Text style={styles.CircleButtonTitle}>{this.props.children}</Text>
+      <View style={[styles.CircleButton, style, {backgroundColor: bgColor}]}>
+        <Text style={[styles.CircleButtonTitle, {color: textColor}]}>{this.props.children}</Text>
       </View>
 
     )
