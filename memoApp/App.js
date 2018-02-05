27008de8ef8fb1d124c8d442a412e6a9eb81a9d6
tomonakar <tomohisa.nak@gmail.com>
2018-02-05 15:10:46 +0900

    メモリストコンポーネントを追加

--- memoApp/App.js
+++ memoApp/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import BodyText from './src/elements/BodyText'
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <BodyText />
       </View>
     );
   }
