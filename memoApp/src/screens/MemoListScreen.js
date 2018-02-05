1ab2b10c1bbcf389a8c17a66fe14bcf22fb32a4a
tomonakar <tomohisa.nak@gmail.com>
2018-02-05 15:48:11 +0900

    メモリスト用スクリーンコンポーネントを作成

---
+++ memoApp/src/screens/MemoListScreen.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {StyleSheet, View} from 'react-native'
+
+import MemoList from '../components/MemoList'
+import CircleButton from '../elements/CircleButton'
+
+class MemoListScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MemoList />
+        <CircleButton>+</CircleButton>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flex: 1,
+  }
+})
+export default MemoListScreen
