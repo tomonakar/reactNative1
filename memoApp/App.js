8ea1bac9f274f2713879db665973b7740bc363ac
tomonakar <tomohisa.nak@gmail.com>
2018-02-05 22:07:00 +0900

    ログイン画面、ユーザ登録画面を追加

--- memoApp/App.js
+++ memoApp/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, View } from 'react-native';
 import Appbar from './src/components/Appbar'
 // import MemoListScreen from './src/screens/MemoListScreen'
 // import MemoDetailScreen from './src/screens/MemoDetailScreen'
-import MemoEditScreen from './src/screens/MemoEditScreen'
+// import MemoEditScreen from './src/screens/MemoEditScreen'
+// import LoginScreen from './src/screens/LoginScreen'
+import SignupScreen from './src/screens/SignupScreen'
 
 export default class App extends React.Component {
   render() {
@@ -13,7 +15,9 @@ export default class App extends React.Component {
         <Appbar />
         {/*<MemoListScreen />*/}
         {/*<MemoDetailScreen />*/}
-        <MemoEditScreen />
+        {/*<MemoEditScreen />*/}
+        {/*<LoginScreen />*/}
+        <SignupScreen />
       </View>
     )
   }
