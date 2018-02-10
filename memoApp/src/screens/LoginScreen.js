4a64b9157cc35a1f734b871ab563d9a35749ac5b
tomonakar <tomohisa.nak@gmail.com>
2018-02-10 17:35:59 +0900

    fix: ログイン画面に戻ることが出来ないように修正

--- memoApp/src/screens/LoginScreen.js
+++ memoApp/src/screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {StyleSheet, View, Text, TextInput, TouchableHighlight} from 'react-native'
 import firebase from 'firebase'
+import { NavigationActions } from 'react-navigation'
 
 class LoginScreen extends React.Component {
   state = {
@@ -10,9 +11,14 @@ class LoginScreen extends React.Component {
 
   handleSubmit() {
     firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-      .then((user) => {
-        console.log('success', user);
-        this.props.navigation.navigate('Home')
+      .then(() => {
+        const resetAction = NavigationActions.reset({
+          index: 0,
+          actions: [
+            NavigationActions.navigate({ routeName: 'Home'})
+          ]
+        })
+        this.props.navigation.dispatch(resetAction)
       })
       .catch((error) => {
         console.log('error', error);
